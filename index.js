51506c98437810a35cc1b8e84b9cdc777cc2de1a
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2021-08-22 12:22:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,24 +6,11 @@ const ws = new WebSocket.Server({ port: PORT || 3000 });
 
 let messages = {}
 
-let newUser = username => {
-    ws.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            let json = {
-                'Action': 'New User',
-                'Username': username
-            }
 
-            client.send(JSON.stringify(json));
-        }
-    });
-}
 
 ws.on('connection', function connection(wss) {
 
     wss.on('message', function incoming(message) {
-        if (message === 'Keep Alive!') return wss.send('Keep Alive!');
-        else if (JSON.parse(message)['Action'] == 'Connected') return newUser(JSON.parse(message)['Username']);
         
         let lessThanFour = message.length < 1 ? true : false;
 
