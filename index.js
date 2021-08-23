512d3a7547f19846c89a0695338a781746e11076
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2021-08-23 14:40:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ const WebSocket = require('ws');
 require('dotenv').config();
 const PORT = process.env.PORT; 
 
-const ws = new WebSocket.Server({ port: PORT || 3000 });
+const wst = new WebSocket.Server({ port: PORT || 3000 });
 
 let messages = {}
 
-
-
-ws.on('connection', function connection(wss) {
-
-    wss.on('message', function incoming(message) {
+let newUser = username => {
+    wst.clients.forEach(function each(client) {
         
+        if (client.readyState === WebSocket.OPEN) {
+            let json = {
+                'Action': 'New User',
+                'Username': username
+            }
+
+            client.send(JSON.stringify(json));
+            
+        }
+    });
+}
+
+wst.on('connection', function connection(wss) {
+    wss.on('message', function incoming(message) {
+        if (message === 'KA') return wss.send('KA');
+        else if (JSON.parse(message)['Action'] == 'Connection') return newUser(JSON.parse(message)['Username']);
         let lessThanFour = message.length < 1 ? true : false;
 
         ws.clients.forEach(function each(client) {
