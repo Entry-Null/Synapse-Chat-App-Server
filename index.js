a692cd9ab45f2ca5e1809a5d17e6d601ea6f3fd6
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2021-08-23 15:09:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-const WebSocket = require('ws');
-require('dotenv').config();
-const PORT = process.env.PORT; 
+const js_socket = require("ws")
 
-const wst = new WebSocket.Server({ port: PORT || 3000 });
 
-let messages = {}
+// Connection 
+let sock_cache = []
+const sock = new js_socket.Server({port:88})
 
-let newUser = username => {
-    wst.clients.forEach(function each(client) {
-        
-        if (client.readyState === WebSocket.OPEN) {
-            let json = {
-                'Action': 'New User',
-                'Username': username
+// Events 
+sock.on("connection",function(client){
+    console.log('New Connection')
+    client.on('message',function(message){
+        sock.clients.forEach(function each(client) {
+            if (cli !== sock) {
+              client.send(message);
             }
-
-            client.send(JSON.stringify(json));
-            
-        }
-    });
-}
-
-wst.on('connection', function connection(wss) {
-    wss.on('message', function incoming(message) {
-        if (message === 'KA') return wss.send('KA');
-        else if (JSON.parse(message)['Action'] == 'Connection') return newUser(JSON.parse(message)['Username']);
-        let lessThanFour = message.length < 1 ? true : false;
-
-        ws.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN && !lessThanFour) client.send(message);
-        });
-    });
-});
+        })
+    })
+})
